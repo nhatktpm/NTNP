4f7298fba6f9aa5fba9c11e9240340d783b0a086
Nhat.Nguyen.PTN <nhat.nguyen@ptnglobalcorp.com>
2022-12-05 22:35:51 +0700

    feature(1): setup EF core assembly v2

--- server/NTNP.API/NTNP.EFCore/Context/NTNPContext.cs
+++ server/NTNP.API/NTNP.EFCore/Context/NTNPContext.cs
@@ -1,15 +1,36 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.EntityFrameworkCore;
+using NTNP.EFCore.Models.Users;
 
 namespace NTNP.EFCore.Context
 {
-    public class NTNPContext : DbContext
+    public partial class NTNPContext : DbContext
     {
         private readonly string _connectionString;
         public NTNPContext(string connectionString)
         {
             _connectionString = connectionString;
         }
+        public NTNPContext(DbContextOptions<NTNPContext> options)
+            : base(options)
+        {
+        }
 
+        public virtual DbSet<User> Bookings { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(_connectionString);
+            }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("Latin1_General_CI_AS");
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(NTNPContext).Assembly);
+            modelBuilder.HasSequence<int>("AccpacBatchNumberSequence");
+            OnModelCreatingPartial(modelBuilder);
+        }
 
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
 }
