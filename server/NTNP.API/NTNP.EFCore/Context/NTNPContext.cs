8c5c843783ca6598c206b8e06b94edd702376cc5
Nhat.Nguyen.PTN <nhat.nguyen@ptnglobalcorp.com>
2022-12-05 22:35:14 +0700

    feature(1): setup EF core assembly

---
+++ server/NTNP.API/NTNP.EFCore/Context/NTNPContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace NTNP.EFCore.Context
+{
+    public class NTNPContext : DbContext
+    {
+        private readonly string _connectionString;
+        public NTNPContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
+    }
+}
---
+++ server/NTNP.API/NTNP.EFCore/Models/Users/UserConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NTNP.EFCore.Models.Users
+{
+    internal class UserConfig : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable(nameof(User));
+        }
+    }
+}
