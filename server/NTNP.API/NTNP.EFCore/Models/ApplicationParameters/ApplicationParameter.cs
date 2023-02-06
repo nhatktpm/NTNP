19c6a15b6d99d042d246fda46aa17add7793da56
Nhat.Nguyen.PTN <nhat.nguyen@ptnglobalcorp.com>
2023-02-06 15:09:05 +0700

    Add ApplicationParameter Table and Repository

---
+++ server/NTNP.API/NTNP.EFCore/Models/ApplicationParameters/ApplicationParameter.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTNP.EFCore.Models.ApplicationParameters
+{
+    public class ApplicationParameter
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        /// <example>1</example>
+        [Key]
+        public int Id { get; set; }
+        /// <summary>
+        /// Parameter Name
+        /// </summary>
+        /// <example>MaxInvalidPasswordAttempts</example>
+        public string ParameterName { get; set; } = null!;
+        /// <summary>
+        /// Value of parameter
+        /// </summary>
+        /// <example>4</example>
+        public string ParameterValue { get; set; } = null!;
+    }
+}
---
+++ server/NTNP.API/NTNP.EFCore/Models/ApplicationParameters/ApplicationParameterConfig.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NTNP.EFCore.Models.ApplicationParameters
+{
+    internal class ApplicationParameterConfig : IEntityTypeConfiguration<ApplicationParameter>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationParameter> builder)
+        {
+            builder.ToTable(nameof(ApplicationParameter));
+        }
+    }
+}
